111c1228c858140565f9006ebde851768aa01fd5
soykim <soykim@payletter.com>
2019-07-29 09:21:39 +0900

    주석 제거

--- src/main/java/com/kakaopay/ServiceConfig.java
+++ src/main/java/com/kakaopay/ServiceConfig.java
@@ -19,40 +19,4 @@ import java.beans.PropertyVetoException;
 @ComponentScan(basePackages = "com.kakaopay")
 @EnableJpaRepositories(basePackages = "com.kakaopay")
 public class ServiceConfig {
-
-//    @Bean
-//    public DataSource dataSource(){
-//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-//        try{
-//            dataSource.setDriverClass("org.mariadb.jdbc.Driver");
-//        }catch (PropertyVetoException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        dataSource.setJdbcUrl("jdbc:mariadb://localhost:3306/jpastart?characterEncoding=utf8");
-//        dataSource.setUser("jpauser");
-//        dataSource.setPassword("jpapass");
-//        return dataSource;
-//    }
-//
-//    @Bean
-//   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//       LocalContainerEntityManagerFactoryBean emfBean = new LocalContainerEntityManagerFactoryBean();
-//       emfBean.setDataSource(dataSource());
-//       emfBean.setPersistenceUnitName("kakaopay");
-//
-//       HibernateJpaVendorAdapter jva = new HibernateJpaVendorAdapter();
-//       jva.setDatabase(Database.MYSQL);
-//       jva.setShowSql(true);
-//
-//       emfBean.setJpaVendorAdapter(jva);
-//       return emfBean;
-//   }
-//
-//   @Bean
-//   public JpaTransactionManager transactionManager(EntityManagerFactory emf){
-//       JpaTransactionManager txMgr = new JpaTransactionManager();
-//       txMgr.setEntityManagerFactory(emf);
-//       return txMgr;
-//   }
 }
